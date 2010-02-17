aad2e4076100bfc909be6c669bc0029edea0f2d1
Mark Proctor <mdproctor@gmail.com>
2010-02-17 10:14:40 +0000

    -JBRULES-2351 OSGi Ready -Added BPMN2 to OSGi tests
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31706 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- osgi-bundles/org.drools.osgi.test/src/test/java/org/drools/osgi/integrationtests/BPMN2OsgiTest.java
+++ osgi-bundles/org.drools.osgi.test/src/test/java/org/drools/osgi/integrationtests/BPMN2OsgiTest.java
@@ -1,14 +1,9 @@
 package org.drools.osgi.integrationtests;
 
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.drools.KnowledgeBase;
-import org.drools.KnowledgeBaseConfiguration;
-import org.drools.KnowledgeBaseFactory;
 import org.drools.KnowledgeBaseFactoryService;
-import org.drools.bpmn2.SimpleBPMNProcessTest;
 import org.drools.bpmn2.xml.BPMN2SemanticModule;
 import org.drools.bpmn2.xml.BPMNDISemanticModule;
 import org.drools.bpmn2.xml.BPMNSemanticModule;
@@ -17,11 +12,9 @@ import org.drools.bpmn2.xpath.XPathDialectConfiguration;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderConfiguration;
 import org.drools.builder.KnowledgeBuilderError;
-import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.KnowledgeBuilderFactoryService;
 import org.drools.builder.ResourceType;
 import org.drools.compiler.PackageBuilderConfiguration;
-import org.drools.io.ResourceFactory;
 import org.drools.io.ResourceFactoryService;
 import org.drools.osgi.test.AbstractDroolsSpringDMTest;
 import org.drools.ruleflow.core.RuleFlowProcess;
@@ -29,13 +22,7 @@ import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.drools.util.ServiceRegistry;
 import org.drools.xml.XmlProcessReader;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
-import org.springframework.osgi.util.OsgiStringUtils;
-import org.test.Cheese;
-import org.test.Person;
-import org.test.decisiontable.Dummy;
 
 public class BPMN2OsgiTest extends AbstractDroolsSpringDMTest {
 
