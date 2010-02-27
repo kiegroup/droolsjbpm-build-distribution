b2d1bee0426c5ced80678ebae7ee2fa700bab25b
Mark Proctor <mdproctor@gmail.com>
2010-02-27 05:05:25 +0000

    JBRULES-2351 OSGi Ready -deploy no longer installed wrapper jars -reverted BPMN2 tests from using the BPMN2 ResourceType as it's not configured correctly for OSGi environments.
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@31855 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- osgi-bundles/org.drools.osgi.test/src/test/java/org/drools/osgi/integrationtests/BPMN2OsgiTest.java
+++ osgi-bundles/org.drools.osgi.test/src/test/java/org/drools/osgi/integrationtests/BPMN2OsgiTest.java
@@ -1,54 +1,80 @@
 package org.drools.osgi.integrationtests;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactoryService;
+import org.drools.bpmn2.xml.BPMN2SemanticModule;
+import org.drools.bpmn2.xml.BPMNDISemanticModule;
+import org.drools.bpmn2.xml.BPMNSemanticModule;
+import org.drools.bpmn2.xml.XmlBPMNProcessDumper;
+import org.drools.bpmn2.xpath.XPathDialectConfiguration;
 import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderConfiguration;
 import org.drools.builder.KnowledgeBuilderError;
 import org.drools.builder.KnowledgeBuilderFactoryService;
 import org.drools.builder.ResourceType;
+import org.drools.compiler.PackageBuilderConfiguration;
+import org.drools.compiler.xml.XmlProcessReader;
 import org.drools.io.ResourceFactoryService;
 import org.drools.osgi.test.AbstractDroolsSpringDMTest;
+import org.drools.ruleflow.core.RuleFlowProcess;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
 import org.drools.util.ServiceRegistry;
-import org.osgi.framework.Bundle;
 import org.osgi.framework.ServiceReference;
 
 public class BPMN2OsgiTest extends AbstractDroolsSpringDMTest {
 
     protected void onSetUp() throws Exception {
+        
     }
 
     protected void onTearDown() throws Exception {
+        
     }
 
-	public void testMinimalProcess() throws Exception {		
-		KnowledgeBase kbase = createKnowledgeBase("BPMN2-MinimalProcess.xml");
-		StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
-		ProcessInstance processInstance = ksession.startProcess("Minimal");
-		assertTrue(processInstance.getState() == ProcessInstance.STATE_COMPLETED);
-	}
-	
-	private KnowledgeBase createKnowledgeBase(String process) throws Exception {
+    public void testMinimalProcess() throws Exception {     
+        KnowledgeBase kbase = createKnowledgeBase("BPMN2-MinimalProcess.xml");
+        StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+        ProcessInstance processInstance = ksession.startProcess("Minimal");
+        assertTrue(processInstance.getState() == ProcessInstance.STATE_COMPLETED);
+    }
+    
+    private KnowledgeBase createKnowledgeBase(String process) throws Exception {
+        
         ServiceReference serviceRef = bundleContext.getServiceReference( ServiceRegistry.class.getName() );
         ServiceRegistry registry = (ServiceRegistry) bundleContext.getService( serviceRef );
+
         KnowledgeBuilderFactoryService knowledgeBuilderFactoryService = registry.get( KnowledgeBuilderFactoryService.class );
         KnowledgeBaseFactoryService knowledgeBaseFactoryService = registry.get( KnowledgeBaseFactoryService.class );
         ResourceFactoryService resourceFactoryService = registry.get( ResourceFactoryService.class );
         
-		KnowledgeBuilder kbuilder = knowledgeBuilderFactoryService.newKnowledgeBuilder();
-		kbuilder.add(resourceFactoryService.newClassPathResource(process, BPMN2OsgiTest.class), ResourceType.BPMN2);
-		if (!kbuilder.getErrors().isEmpty()) {
-			for (KnowledgeBuilderError error: kbuilder.getErrors()) {
-				System.err.println(error);
-			}
-			throw new IllegalArgumentException("Errors while parsing knowledge base");
-		}
-		KnowledgeBase kbase = knowledgeBaseFactoryService.newKnowledgeBase();
-		kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
-		return kbase;
-	}	
+        KnowledgeBuilderConfiguration conf = knowledgeBuilderFactoryService.newKnowledgeBuilderConfiguration();
+        ((PackageBuilderConfiguration) conf).initSemanticModules();
+        ((PackageBuilderConfiguration) conf).addSemanticModule(new BPMNSemanticModule());
+        ((PackageBuilderConfiguration) conf).addSemanticModule(new BPMN2SemanticModule());
+        ((PackageBuilderConfiguration) conf).addSemanticModule(new BPMNDISemanticModule());
+        ((PackageBuilderConfiguration) conf).addDialect("XPath", new XPathDialectConfiguration());        
+        
+        XmlProcessReader processReader = new XmlProcessReader(
+            ((PackageBuilderConfiguration) conf).getSemanticModules());
+        RuleFlowProcess p = (RuleFlowProcess)
+            processReader.read(BPMN2OsgiTest.class.getResourceAsStream(process));               
+        
+        KnowledgeBuilder kbuilder = knowledgeBuilderFactoryService.newKnowledgeBuilder(conf);
+        
+        kbuilder.add(resourceFactoryService.newReaderResource(
+            new StringReader(XmlBPMNProcessDumper.INSTANCE.dump(p))), ResourceType.DRF);
+        if (!kbuilder.getErrors().isEmpty()) {
+            for (KnowledgeBuilderError error: kbuilder.getErrors()) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Errors while parsing knowledge base");
+        }
+        KnowledgeBase kbase = knowledgeBaseFactoryService.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }   
 
 }
