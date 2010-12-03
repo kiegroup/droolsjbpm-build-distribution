f75003e4bd48aebf6ca35e6de484566e6d27279b
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2010-12-03 10:02:42 +0000

    documentation for shell script
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@36186 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- src/script/detectSplitPackagesOverModules.sh
+++ src/script/detectSplitPackagesOverModules.sh
@@ -1,4 +1,12 @@
 #! /bin/sh
+# Using the same package in multiple modules (jars/wars) is bad for OSGi and Jigsaw.
+# This script detects such split packages.
+# FIXME it outputs false positives (but no false negatives, so it's reliable):
+# grouping packages that have no direct classes, such as org and org.drools and org.drools.planner
 # @author Geoffrey De Smet
 
+echo "split packages in src/main/java:"
 ls -R | grep "src/main/java/" | sed "s/\(.*\/src\/main\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
+
+echo "split packages in src/test/java:"
+ls -R | grep "src/test/java/" | sed "s/\(.*\/src\/test\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
